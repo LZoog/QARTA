c95117f227f15772fb9e32d0ef6170cec56d6723
Lauren Zugai <lauren@zugai.com>
2018-04-27 15:07:11 -0500

    Async/await-ify server.js

--- server.js
+++ server.js
@@ -1,21 +1,21 @@
 import Nightmare from 'nightmare'
 const nightmare = Nightmare()
 
-nightmare
-  .goto('')
-  .evaluate(() => {
+takeScreenshot().catch(console.error)
+
+async function takeScreenshot() {
+  const nightmare = Nightmare()
+
+  const dimensions = await nightmare.goto('').evaluate(() => {
     const html = document.querySelector('html')
     return {
       height: html.scrollHeight + 100,
       width: html.scrollWidth
     }
   })
-  .then(dimensions => nightmare
+  await nightmare
     .viewport(dimensions.width, dimensions.height)
     .screenshot('./test.png')
-  )
-  .then(() => {
-    nightmare.end(() => {
-      console.log('done')
-    })
-  })
+  await nightmare
+    .end(() => console.log('done'))
+}
