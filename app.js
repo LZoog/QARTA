bc869c3d047ebb60fac6b0dd79801c05673344ef
Lauren Zugai <lauren@zugai.com>
2018-05-07 14:02:03 -0500

    move screenshot functions to screenshot.js, remove helpers.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-import Nightmare from 'nightmare'
+
 import { paths, urls } from './config'
-import { asyncForEach } from './server/helpers'
+import { parallelScreenshots } from './server/screenshot'
 import './server/timestamp'
 
 
@@ -10,44 +10,9 @@ import './server/timestamp'
 
   for (const pathObject of paths) {
     (async () => {
-      const screenshots = await parallelScreenshots(pathObject, timestamp)
+      const screenshots = await parallelScreenshots(urls, pathObject, timestamp)
 
       console.log(screenshots)
     })()
   }
 })()
-
-async function parallelScreenshots(pathObject, timestamp) {
-  // at present, only two urls are allowed in config
-  const screenshot1 = screenshot(urls[0], pathObject, timestamp);
-  const screenshot2 = screenshot(urls[1], pathObject, timestamp);
-
-  return {
-    screenshot1: await screenshot1,
-    screenshot2: await screenshot2,
-  }
-}
-
-async function screenshot(urlObject, pathObject, timestamp) {
-  console.log('screenshot fired')
-  const nightmare = Nightmare()
-  const { url, name: urlName } = urlObject
-  const { path, name: pathName } = pathObject
-  const pageUrl = `${url}/${path}`
-  const screenshotName = `${urlName}_${pathName}_${timestamp}`
-
-  const dimensions = await nightmare.goto(pageUrl).evaluate(() => {
-    const html = document.querySelector('html')
-    return {
-      height: html.scrollHeight + 100,
-      width: html.scrollWidth,
-    }
-  })
-  await nightmare
-  .viewport(dimensions.width, dimensions.height)
-  .screenshot(`./screenshots/${screenshotName}.png`)
-  await nightmare
-    .end(() => { console.log('screenshot finished ', screenshotName) } )
-
-  return screenshotName
-}
---
+++ server/screenshot.js
@@ -0,0 +1,37 @@
+import Nightmare from 'nightmare'
+
+export async function parallelScreenshots(urls, pathObject, timestamp) {
+  // at present, only two urls are allowed in config
+  const screenshot1 = screenshot(urls[0], pathObject, timestamp);
+  const screenshot2 = screenshot(urls[1], pathObject, timestamp);
+
+  return {
+    screenshot1: await screenshot1,
+    screenshot2: await screenshot2,
+  }
+}
+
+async function screenshot(urlObject, pathObject, timestamp) {
+  const nightmare = Nightmare()
+  const { url, name: urlName } = urlObject
+  const { path, name: pathName } = pathObject
+  const pageUrl = `${url}/${path}`
+  const screenshotName = `${urlName}_${pathName}_${timestamp}`
+
+  try {
+    const dimensions = await nightmare.goto(pageUrl).evaluate(() => {
+      const html = document.querySelector('html')
+      return {
+        height: html.scrollHeight + 100,
+        width: html.scrollWidth,
+      }
+    })
+    await nightmare
+    .viewport(dimensions.width, dimensions.height)
+    .screenshot(`./screenshots/${screenshotName}.png`)
+    await nightmare
+      .end(() => console.log('screenshot finished ', screenshotName))
+  } catch(error) { return error }
+
+  return screenshotName
+}
